2efda9e641f26da07d4a6f7f940e980a19ab4b66
Daniel Micher <michermd@yahoo.com>
2016-04-17 22:10:02 -0500

    Implemento Signup y login a la base de datos

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ Bundler.require
 configure do
   db = Sequel.connect('sqlite://db.db')
 
-  db.create_table? :user do
+  db.create_table? :users do
     primary_key   :id
 
     String    :name
     String    :lastname
-    String    :user_name
+    String    :user_name, unique: true
     String    :password
     String    :email
     String    :phone
@@ -32,58 +32,79 @@ get "/" do
   # además, mostrar un link al registro
 
   erb :splash
-
 end
 
-get "/sign_up" do
 
+
+get "/sign_up" do
   #cuando el usuario no tiene cuenta se registra con
   #user_type ,name ,password, numero_telefono
   #formulario para llenar los datos y un boton de submit
   #si el usuario ya tiene cuenta link a login
 
   erb :sign_up
-
 end
+
+
 
 post "/sign_up" do
   #se verifica el registro y se crea el usuario
   # redirigimos el usuario a iniciar sesión
   data = params
-  data[:password] = BCrypt::Password.create(data[:pass1])
 
-  dbu = settings.db[:user]
-  dbu.insert(data)
-    lastname.delete
+  if params[:pass1] != params[:pass2]
+    return redirect to "/sign_up"
+  end
 
+  data[:password] = BCrypt::Password.create(params[:pass1])
+  data.delete 'pass1'
+  data.delete 'pass2'
+
+  dbu = settings.db[:users]
+
+  begin
+    dbu.insert(data)
+  rescue Sequel::UniqueConstraintViolation => e
+    puts e
+    return "Ese nombre de usuario ya existe"
+  else
+    redirect to "/login"
+  end
+  
 end
+
+
 
 get "/login" do
   #el usuario se registra con sus datos
   #formulario de username y password con boton de submit
 
-  erb :login
+  erb :login, locals: {error: params[:error]}
 end
+
+
 
 post "/login" do
   # el sistema veifica los datos del login
   # si es válido el usuario: te manda selección de team
-  # si no te regresa a /login?fail=true
+  # si no te regresa a /login?error=true
   
-  dbu = settings.db[:user]
+  dbu = settings.db[:users]
   user = dbu.filter({user_name: params[:user_name]}).first
 
-  if user && BCrypt::Password.new(user.password) == params[:password]
+  puts user[:password]
+
+  if user && BCrypt::Password.new(user[:password]) == params[:password]
     session[:user] = user
     redirect to "/emergency"
   else
-    redirect to "/login"
+    redirect to "/login?error=true"
   end
-
 end
 
-get "/emergency" do
 
+
+get "/emergency" do
   # muestra botón para crear emergency
   
   unless session[:user]
